db3db80f0a1b4462ffeb99d59f6f578d44216332
mattimus11 <78524878+mattimus11@users.noreply.github.com>
2022-12-02 13:56:50 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -6,8 +6,8 @@ export const dynasty = true; // true for dynasty leagues, false for redraft and 
 export const enableBlog = false; //requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
-export const homepageText = 
-;
+export const homepageText =' 
+';
 
 /*   STEP 3   */
 /*
