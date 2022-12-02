f3e1c1533c737f7638c85462e4a610f64a9bc207
mattimus11 <78524878+mattimus11@users.noreply.github.com>
2022-12-02 13:36:58 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "854795474719334400"; // your league ID
 export const leagueName = "League of Leagues"; // your league name
-export const dues = ; // (optional) used in template constitution page
+export const dues = 0; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; //requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
